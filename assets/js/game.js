c4168305b3f5dedf8174e7bcb51a2c177adb403d
Mario Essig <mario..a.essig@gmail.com>
2020-08-14 10:02:20 -0500

    prevent null player name entry

--- assets/js/game.js
+++ assets/js/game.js
@@ -85,6 +85,7 @@ var startGame = function () {
             playerInfo.money = 10;
 
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            debugger;
 
             var pickedEnemyObj = enemyInfo[i];
 
@@ -168,9 +169,21 @@ var randomNumber = function (min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function () {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is"  + name);
+    return name;
+};
+
 // Set playerInfo object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
